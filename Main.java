35c57a31347b586f5ba113b174cce9dab118e2b1
Owen Beyer <156833692+0beyer@users.noreply.github.com>
2025-09-09 15:33:47 -0500

    Fixed quickSortInsSort and documented code

--- Main.java
+++ Main.java
@@ -4,43 +4,43 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        int size = 10000;
+        int size = 1000;
 
         System.out.println(" ");
         System.out.println("newArrOrd Sorting:");
         mergeSort(newArrOrd(size));
-        // quickSort(newArrOrd(size));
+        quickSort(newArrOrd(size));
         quickSortRanPiv(newArrOrd(size));
         quickSortMedPiv(newArrOrd(size));
         quickSortInsSort(newArrOrd(size));
 
-        // System.out.println(" ");
-        // System.out.println("newArrRevOrd Sorting:");
-        // mergeSort(newArrRevOrd(size));
-        // quickSort(newArrRevOrd(size));
-        // quickSortRanPiv(newArrRevOrd(size));
-        // quickSortMedPiv(newArrRevOrd(size));}
-        // // quickSortInsSort(newArrRevOrd(size));
+        System.out.println(" ");
+        System.out.println("newArrRevOrd Sorting:");
+        mergeSort(newArrRevOrd(size));
+        quickSort(newArrRevOrd(size));
+        quickSortRanPiv(newArrRevOrd(size));
+        quickSortMedPiv(newArrRevOrd(size));
+        quickSortInsSort(newArrRevOrd(size));
 
-        // System.out.println(" ");
-        // System.out.println("newArrRand Sorting:");
-        // mergeSort(newArrRand(size));
-        // quickSort(newArrRand(size));
-        // quickSortRanPiv(newArrRand(size));
-        // quickSortMedPiv(newArrRand(size));
-        // // quickSortInsSort(newArrRan(size));
+        System.out.println(" ");
+        System.out.println("newArrRand Sorting:");
+        mergeSort(newArrRand(size));
+        quickSort(newArrRand(size));
+        quickSortRanPiv(newArrRand(size));
+        quickSortMedPiv(newArrRand(size));
+        quickSortInsSort(newArrRand(size));
 
-        // System.out.println(" ");}
-        // System.out.println("newArrSort Sorting:");
-        // mergeSort(newArrSort(size));
-        // quickSort(newArrSort(size));
-        // quickSortRanPiv(newArrSort(size));
-        // quickSortMedPiv(newArrSort(size));
-        // // quickSortInsSort(newArrSort(size));
+        System.out.println(" ");
+        System.out.println("newArrSort Sorting:");
+        mergeSort(newArrSort(size));
+        quickSort(newArrSort(size));
+        quickSortRanPiv(newArrSort(size));
+        quickSortMedPiv(newArrSort(size));
+        quickSortInsSort(newArrSort(size));
 
     }
         
-
+    // these are variations of the sort function that also prints and resets count
     public static void mergeSort(TestInteger[] arr) {
         Arrays.sort(arr);
         System.out.println("MergeSort Count: " + TestInteger.getCounter());
@@ -61,8 +61,6 @@ public class Main {
         System.out.println("Sorted: " + isSorted(arr));
         TestInteger.resetCounter();
     }
-
-    
         
     public static void quickSortMedPiv(TestInteger[] arr) {
         quickSortRecursiveMedPiv(arr, 0, arr.length - 1);
@@ -94,6 +92,7 @@ public class Main {
         }
     }
 
+    // will create an an array with 10 subarrays that are in order
     public static TestInteger[] newArrOrd(int size) {
         TestInteger[] base = orderedArrayTemp(size);
         for (int i = 0; i < 9; i++) {
@@ -102,6 +101,7 @@ public class Main {
         return base;
     }
 
+    // revered order subarrays
     public static TestInteger[] newArrRevOrd(int size) {
         TestInteger[] base = revOrderedArrayTemp(size);
         for (int i = 0; i < 9; i++) {
@@ -110,6 +110,7 @@ public class Main {
         return base;
     }
 
+    // sorted array
     public static TestInteger[] newArrSort(int size) {
         TestInteger[] base = new TestInteger[10 * size];
         populateRan(base);
@@ -118,12 +119,14 @@ public class Main {
         return base;
     }
 
+    // random ordered array
     public static TestInteger[] newArrRand(int size) {
         TestInteger[] base = new TestInteger[10 * size];
         populateRan(base);
         return base;
     }
 
+    // temp sorted array made for newArrOrd and newArrRevOrd
     public static TestInteger[] orderedArrayTemp(int size) {
         TestInteger[] base = new TestInteger[size];
         populateRan(base);
@@ -141,6 +144,7 @@ public class Main {
         return base;
     }
 
+    // helper function that reverses an array
     static TestInteger[] reverse(TestInteger[] a) {
         TestInteger[] b = new TestInteger[a.length];
         int j = a.length;
@@ -151,6 +155,7 @@ public class Main {
         return b;
     }
 
+    // populates an array with random elements from 0 to a million
     public static TestInteger[] populateRan(TestInteger[] arr) {
         Random rand = new Random();
         for (int i = 0; i < arr.length; i++) {
@@ -159,12 +164,14 @@ public class Main {
         return arr;
     }
 
+    // concatenates two arrays
     public static TestInteger[] concatenate(TestInteger[] a, TestInteger[] b) {
         TestInteger[] both = Stream.concat(Arrays.stream(a), Arrays.stream(b)).toArray(TestInteger[]::new);
         TestInteger.resetCounter();
         return both;
     }
 
+    // quick sort v1
     public static void quickSortRecursive(TestInteger[] arr, int low, int high) {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
@@ -173,6 +180,7 @@ public class Main {
         }
     }
 
+    // quick sort with a random pivot
     public static void quickSortRecursiveRanPiv(TestInteger[] arr, int low, int high) {
         if (low < high) {
             int pivotIndex = partitionRan(arr, low, high);
@@ -181,26 +189,44 @@ public class Main {
         }
     }
 
+    // quick sort with a median pivot of 3 random pivots, and a random pivot when the
+    // subarray has length of 30
     public static void quickSortRecursiveMedPiv(TestInteger[] arr, int low, int high) {
         if (low < high) {
-            int pivotIndex = partitionMed(arr, low, high);
-            quickSortRecursiveMedPiv(arr, low, pivotIndex - 1);
-            quickSortRecursiveMedPiv(arr, pivotIndex + 1, high);
+            if(high - low > 30) {
+                int pivotIndex = partitionMid(arr, low, high);
+                quickSortRecursiveMedPiv(arr, low, pivotIndex - 1);
+                quickSortRecursiveMedPiv(arr, pivotIndex + 1, high);
+            }
+            else {
+                int pivotIndex = partitionRan(arr, low, high);
+                quickSortRecursiveMedPiv(arr, low, pivotIndex - 1);
+                quickSortRecursiveMedPiv(arr, pivotIndex + 1, high);
+            }
         }
     }
 
+    // quick sort using median pivot, and returns after subarray is 2 long
+    // it will then call insertion sort to finish sorting the array
     public static void quickSortRecursiveInsSort(TestInteger[] arr, int low, int high) {
-        if (low < high && high - low > 8) {
-            int pivotIndex = partitionMed(arr, low, high);
-            quickSortRecursiveInsSort(arr, low, pivotIndex - 1);
-            quickSortRecursiveInsSort(arr, pivotIndex + 1, high);
+        if (low < high) {
+            if (high - low > 30) {
+                int pivotIndex = partitionMid(arr, low, high);
+                quickSortRecursiveInsSort(arr, low, pivotIndex - 1);
+                quickSortRecursiveInsSort(arr, pivotIndex + 1, high);
+            }
+            if (high - low <= 30 && high - low > 3) {
+                int pivotIndex = partitionRan(arr, low, high);
+                quickSortRecursiveInsSort(arr, low, pivotIndex - 1);
+                quickSortRecursiveInsSort(arr, pivotIndex + 1, high);
+            }
+            else {
+                return;
+            }
         }
-        else {
-            return;
-        }
-        // insertionSort(arr);
     }
 
+    // insertion sort
     public static void insertionSort(TestInteger[] arr) {
     
         int n = arr.length;
@@ -214,10 +240,9 @@ public class Main {
             }
             arr[j + 1] = key;
         }
-    }
+    }  
 
-  
-
+    // helper function for partitioning
     private static int partition(TestInteger[] arr, int low, int high) {
         TestInteger pivot = arr[high];
         int i = low - 1;
@@ -232,6 +257,7 @@ public class Main {
         return i + 1;
     }
 
+    // helper function that uses a random pivot
     private static int partitionRan(TestInteger[] arr, int low, int high) {
         random(arr, low, high);
         TestInteger pivot = arr[high];
@@ -247,7 +273,8 @@ public class Main {
         return i + 1;
     }
 
-    private static int partitionMed(TestInteger[] arr, int low, int high) {
+    // helper function that uses a median of 3 points
+    private static int partitionMid(TestInteger[] arr, int low, int high) {
         randomMid(arr, low, high);
         TestInteger pivot = arr[high];
         int i = low - 1;
@@ -262,6 +289,7 @@ public class Main {
         return i + 1;
     }
 
+    // chooses a random pivot
     static void random(TestInteger[] arr, int low, int high) {
         Random rand = new Random();
         int pivot = rand.nextInt(high - low) + low;
@@ -270,6 +298,7 @@ public class Main {
         arr[high] = temp1;
     }
 
+    // chooses a median of 3 random points
     static void randomMid(TestInteger[] arr, int low, int high) {
         Random rand = new Random();
         int p1 = rand.nextInt(high - low) + low;
@@ -282,6 +311,7 @@ public class Main {
         arr[high] = temp1;
     }
 
+    // helper function that returns the midpoint of 3 given array indexes
     static int midpoint(TestInteger[] arr, int p1, int p2, int p3) {
         if (arr[p1].compareTo(arr[p2]) < 0) {
             if (arr[p2].compareTo(arr[p3]) <= 0)
